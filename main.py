3e8074a07f47a4d619ba986809db564aa4d2af8d
NeuralAIM <75082388+NeuralAIM@users.noreply.github.com>
2021-08-04 16:28:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from time import sleep
 import keyboard as kbd
-print("NBinder for Asyakoshka")
-print("   By NeuralTeam")
+print("NBinder By NeuralTeam")
 sleep(3)
 binds = ["4","5","6","7","8","9","0","u","i","o","p","g","h","j","k","l","m","n"]
 while True:
